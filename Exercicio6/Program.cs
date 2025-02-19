c62ae1bd2d4234716a9ad9e840df04a40cc4a9ac
tamyresrsm <tamyres.rsmagalhaes@senacsp.edu.br>
2025-02-18 21:59:55 -0300

    feat: exercicio 6 completo

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double valorHora;
+            short quantHora;
+
+            Console.Write("Digite o seu Nome");
+            nome = Console.ReadLine()!;
+
+            Console.Write("Digite o numero de Horas tabalhadas: ");
+            quantHora =Convert.ToInt16(Console.ReadLine())!;
+
+            Console.Write("Digite o valor por hora: ");
+            valorHora = Convert.ToDouble(Console.ReadLine())!;
+
+            double total = valorHora * quantHora;
+
+            Console.WriteLine($@"
+                O Valor Total de {nome} é R${total:f2}
+            ");
+
+
+        }
+    }
+}
